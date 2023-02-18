358bfc3009c6c982a0e90137908c67c1a2cd4bdb
Ramon Navarro <89048086+NavarroRamon@users.noreply.github.com>
2023-02-17 19:57:00 -0600

    Update amz.py

--- amz.py
+++ amz.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import re
-
+from fake_useragent import UserAgent
 
 class AmazonTracker:
     """
@@ -13,6 +13,12 @@ class AmazonTracker:
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        # User agent option
+        user_agent = UserAgent()
+        user_agent_string = user_agent.random
+        options.add_argument(f'user-agent={user_agent_string}')
+
+        # Launch with options
         self.driver = webdriver.Chrome(options=options)
 
     def __enter__(self):
@@ -22,6 +28,10 @@ class AmazonTracker:
         self.driver.quit()
 
     def get_price(self, url):
+        """
+        :param url: The url of the product
+        :return: The product price
+        """
         self.driver.get(url)
         string = self.driver.page_source
         pattern = r'<span class="a-offscreen">\$([\d,\.]+)'
@@ -30,3 +40,4 @@ class AmazonTracker:
         except AttributeError:
             price = None
         return price
+    
