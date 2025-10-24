d24eaf7fa766503c797adac1c13fc064a9f90729
ramonnavarro <ramon.navarro3110@alumnos.udg.mx>
2025-10-24 00:10:18 -0600

    code updated

--- amz.py
+++ amz.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import re
 from fake_useragent import UserAgent
+from dotenv import load_dotenv
+
+from selenium.webdriver.common.by import By
+
+import os
+load_dotenv()
 
 class AmazonTracker:
     """
@@ -18,8 +25,11 @@ class AmazonTracker:
         user_agent_string = user_agent.random
         options.add_argument(f'user-agent={user_agent_string}')
 
+        # La forma moderna de pasar el path al driver
+        service = Service(executable_path=os.getenv('CHROMEDRIVER'))
+
         # Launch with options
-        self.driver = webdriver.Chrome(options=options)
+        self.driver =  webdriver.Chrome(service=service, options=options)
 
     def __enter__(self):
         return self
@@ -32,12 +42,15 @@ class AmazonTracker:
         :param url: The url of the product
         :return: The product price
         """
-        self.driver.get(url)
-        string = self.driver.page_source
-        pattern = r'<span class="a-offscreen">\$([\d,\.]+)'
         try:
-            price = re.search(pattern, string).group(1)
-        except AttributeError:
+            self.driver.get(url)
+            child_element = self.driver.find_element(By.ID, "renewedSingleOfferHeader")
+            parent_div = child_element.find_element(By.XPATH,
+                                                    "ancestor::div[contains(@class,'a-section') and contains(@class,'a-spacing-none') and contains(@class,'a-padding-none')]")
+            price = parent_div.find_element(By.ID, "apex_offerDisplay_desktop")
+            price = price.text.split('\n')[0].replace('$', '').replace(',', '')
+            price = int(price)
+        except:
             price = None
         return price
     
--- main.py
+++ main.py
@@ -5,93 +5,65 @@ from time import sleep
 import pandas as pd
 from amz import AmazonTracker
 from discord_webhook import DiscordWebhook
+from file_value import read_value, write_value, delete_value
+
+from modules.discord import send_discord
+from modules.telegram import send_telegram
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # webhook to send the message to discord
-webhook = DiscordWebhook(url='insert_your_url', username='AmazonPriceTracker', content='')
+discordWebhook = os.getenv('DISCORD_WEBHOOK')
+loop = os.getenv('LOOP', False)
+webhook = DiscordWebhook(url=discordWebhook, username='AmazonPriceTracker', content='')
+
+# JASON with the information of the product that you need to track
+# should be created as the next examplo
+# {"name_here" : "https://www.amazon.com.mx/dHTT3DTDMG884&colid=32p" }
 ProductsJSON = "products.json"
-ProductsCSV = "products.csv"
 
 
-def amazon_tracker_fun(data_products, df=pd.DataFrame()) -> pd.DataFrame:
+def amazon_tracker_fun(link):
     """
     :param data_products: list of pairs [{"name": "product_name", "link": "product_url"}, {...}]
     :param df: if you want to concatenate the results in a previous dataframe send it as param, if not just ignore it
     :return: df with the product name product price and datetime of current track
     """
     with AmazonTracker() as amazon_tracker:
-        for element in data_products:
-            name = element.get("name")
-            url = element.get("link")
-            if not name or not url:
-                continue
-            try:
-                date = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-                price = amazon_tracker.get_price(url)
-                if not price:
-                    raise ValueError("Price not found")
-                product = {'Product': name, 'Price': price, 'Date': date}
-                df = pd.concat([df, pd.DataFrame([product])])
-            except (ValueError, AttributeError) as e:
-                print(f"Failed to get price for product {name}: {e} \n {url}")
-            except Exception as e:
-                print(f"An error occurred while processing product {name}: {e} \n {url}")
-    return df
-
-
-def lower_price(df_new: pd.DataFrame, df_old: pd.DataFrame) -> pd.DataFrame:
-    """
-    :param df_new: the dataframe with the new prices
-    :param df_old: the historical dataframe with old prices
-    :return: df_lowers_price, with the products with the lowest price than in df_old (if exists)
-    """
-    # Prepare the minimum value in historical and order by products
-    if df_old.empty:
-        df_old = df_new
-    else:
-        df_old.sort_values(by='Product', inplace=True)
-    df_old_min = df_old.groupby('Product', as_index=False).min()
-    # join dataframes df and df_hist_min on the 'Product' column
-    df_merged = pd.merge(df_old_min, df_new, on='Product', suffixes=('_hist', '_now'))
-    price_diff = df_merged['Price_now'] < df_merged['Price_hist']
-    # filter the merged dataframe to show only the rows where the price is lower
-    df_lowers_price = df_merged[price_diff]
-    return df_lowers_price.drop(columns=['link_hist'])
+        try:
+            price = amazon_tracker.get_price(link)
+            if not price:
+                return False
+        except Exception as e:
+            return False
+    return price
 
 
 def main():
-    # Check if historical file exists, if not just load an empty df
-    if os.path.exists(ProductsCSV):
-        df_hist = pd.read_csv(ProductsCSV)
-    else:
-        df_hist = pd.DataFrame()
+    # Load the input data
+    with open(ProductsJSON, "r") as file:
+        data = json.load(file)
+    df = pd.DataFrame(list(data.items()), columns=["product", "link"])
 
     while True:
-        # Load the input data
-        with open(ProductsJSON, "r") as file:
-            data = json.load(file)
-        products_link = pd.DataFrame(data)
-
         # Track the products
-        df = amazon_tracker_fun(data)
-        df = pd.merge(df, products_link, how='left', left_on="Product", right_on="name")
-        df.drop(columns=['name'], inplace=True)
-
-        # Check for lower price
-        df_lower_price = lower_price(df, df_hist)
-        # You can also add a logic where the price has a minimum value, for example send email.
-        if not df_lower_price.empty:
-            webhook.content = f"{df_lower_price}"
-            webhook.execute()
-
-        # Save the historical results in a csv file
-        df_hist = pd.concat([df, df_hist])
-        df_hist.to_csv(ProductsCSV, index=False)
-
-        # Waiting time between execution, and also can add a breakpoint here to see the results
-        for i in range(400):
-            sleep(1)
-            if i == 500:
-                break
+        for elm in df.itertuples(index=False):
+            value_path = os.path.join('data', f"{elm.product}.txt")
+            price = amazon_tracker_fun(elm.link)
+            if not price:
+                send_telegram(f"Error en tracker {elm.product}")
+            current_value = read_value(value_path)
+            if current_value is None or price < int(current_value):
+                write_value(value_path, price)
+                send_telegram(f"{elm.product}\nCosto minimo {price}\n{elm.link}")
+                send_discord(f"{elm.product}\nCosto minimo {price}\n{elm.link}")
+        if not loop:
+            print('Exiting code')
+            break
+        else:
+            sleep(60*4)
 
 
 if __name__ == "__main__":
